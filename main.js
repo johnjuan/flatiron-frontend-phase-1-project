16d7fcf377ce560b991cc2325ab370b8fb6b9865
Juan Valencia <johnjuan15@gmail.com>
2023-08-07 18:33:04 -0700

    focus feature

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 console.log(`connected`)
+
+const handleFormInputFocus = () => {
+console.log(`focus occurred`)
+}
+
+const searchTermsInput = document.body.querySelector("#search-terms")
+
+searchTermsInput.addEventListener('focus', handleFormInputFocus)
